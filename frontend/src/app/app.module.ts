dad2245512a09521927c926752dfa7ee29a623f4
Juliastah <julia.stahorska@gmail.com>
2021-05-18 01:00:06 +0300

    Added bootstrap and components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,12 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MainPageComponent } from './main-page/main-page.component';
-import {AuthorizationModule} from './authorization/authorization.module';
+import { MainPageComponent } from './Components/main-page/main-page.component';
+import {AuthorizationModule} from './Components/authorization/authorization.module';
+import { ProductsComponent } from './Components/products/products.component';
+import { AuctionsComponent } from './Components/auctions/auctions.component';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
+import { SettingsComponent } from './Components/settings/settings.component';
+import { ProductComponent } from './Components/product/product.component';
+import { AuctionComponent } from './Components/auction/auction.component';
+import { BidComponent } from './Components/bid/bid.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
+    ProductsComponent,
+    AuctionsComponent,
+    ShoppingCartComponent,
+    SettingsComponent,
+    ProductComponent,
+    AuctionComponent,
+    BidComponent,
   ],
   imports: [
     BrowserModule,
