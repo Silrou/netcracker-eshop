705d376af74e7a368f5fa035db9802f44c6ce114
Juliastah <julia.stahorska@gmail.com>
2021-05-19 17:34:14 +0300

    Дбоавлены модули для аукционов и продуктов. В них перенесены соответствующие компоненты.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,31 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
-import {AuthorizationModule} from './authorization/authorization.module';
-import { ProductsComponent } from './products/products.component';
-import { AuctionsComponent } from './auctions/auctions.component';
+import { AuthorizationModule } from './authorization/authorization.module';
+import { AuctionsModule } from './auctions/auctions.module';
+import { ProductsModule } from './products/products.module';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { SettingsComponent } from './settings/settings.component';
-import { ProductComponent } from './product/product.component';
-import { AuctionComponent } from './auction/auction.component';
-import { BidComponent } from './bid/bid.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    ProductsComponent,
-    AuctionsComponent,
-    ShoppingCartComponent,
     SettingsComponent,
-    ProductComponent,
-    AuctionComponent,
-    BidComponent,
+    ShoppingCartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthorizationModule,
+    AuctionsModule,
+    ProductsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
