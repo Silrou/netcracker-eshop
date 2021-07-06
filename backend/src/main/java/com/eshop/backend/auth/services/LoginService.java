82586e70aa14e896b21c7c473cc3d12b80b6fe17
Mykola Chernetskiy <koularchear@gmail.com>
2021-07-06 16:27:11 +0300

    add service

---
+++ backend/src/main/java/com/eshop/backend/auth/services/LoginService.java
@@ -0,0 +1,8 @@
+package com.eshop.backend.auth.services;
+
+import com.eshop.backend.auth.dto.LoginRequestDTO;
+import com.eshop.backend.user.dao.models.AuthorizedUserModel;
+
+public interface LoginService {
+    AuthorizedUserModel login(LoginRequestDTO request);
+}
