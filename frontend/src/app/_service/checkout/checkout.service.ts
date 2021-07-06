3185da9a0214b707503400eea69a3ba2cdb62afd
Mykola Chernetskiy <koularchear@gmail.com>
2021-07-06 14:04:18 +0300

    checkout

---
+++ frontend/src/app/_service/checkout/checkout.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../../_model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CheckoutService {
+
+  constructor(private http: HttpClient) { }
+
+  private checkoutUrl = 'http://localhost:8081/checkout';
+
+  getDeliveryHours(date: Date): Observable<any> {
+    const params = new HttpParams().set('date', JSON.stringify(date));
+    const url = `${this.checkoutUrl}/hours`;
+    return this.http.get<any>(url, {params});
+  }
+
+  createOrder(result: any): Observable<any> {
+    // const params = new HttpParams().set('date', JSON.stringify(result));
+    const url = `${this.checkoutUrl}/order`;
+    return this.http.post<any>(url, result);
+  }
+
+  getUserById(id: number): Observable<User> {
+    const url = `${this.checkoutUrl}/user-info/${id}`;
+    return this.http.get<User>(url);
+  }
+}
